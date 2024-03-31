663c679ef0a9ed1162e0ea780dbbe3f2bf957b29
Jun Ji <junji64@gmail.com>
2024-03-31 12:04:47 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import streamlit as st
 from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
-st.title("This is a title")
-content = st.text_input("시의 주제를 제시해주세요.")
+st.title("Quiz Generator")
+content = st.text_input("Context fo a Quiz")
 
-if st.button("시 작성 요청"):
-    with st.spinner("시 작성 중 ..."):
-        result = chat_model.predict(content + "에 대한 시를 써줘") 
+if st.button("Make a Quiz"):
+    with st.spinner("Making a quiz ..."):
+        prompt = '''
+                Create a quiz based on the context of {content}. 
+                Include a variety of question types such as multiple choice, true/false, and short answer 
+                to test the participants' understanding and knowledge depth.
+                '''
+        result = chat_model.predict(prompt) 
     st.write(result)
