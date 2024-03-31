becfeb22147ff42d3ee357fa52739e481155d0e1
Jun Ji <junji64@gmail.com>
2024-03-31 12:11:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 st.title("Quiz Generator")
-content = st.text_input("Context fo a Quiz")
+content = st.text_input("Context for Quizes")
 
-if st.button("Make a Quiz"):
-    with st.spinner("Making a quiz ..."):
+if st.button("Make Quizes"):
+    with st.spinner("Making quizes ..."):
         prompt = "Create a quiz based on the context of " + content + \
                  ". Include a variety of question types such as multiple choice, true/false, and short answer" + \
                  " to test the participants understanding and knowledge depth."
