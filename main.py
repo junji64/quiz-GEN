97370808e09f7efdf019defb18e9c44174ea1cd9
Jun Ji <junji64@gmail.com>
2024-03-31 11:48:46 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+st.title("This is a title")
+content = st.text_input("시의 주제를 제시해주세요.")
+
+if st.button("시 작성 요청"):
+    with st.spinner("시 작성 중 ..."):
+        result = chat_model.predict(content + "에 대한 시를 써줘") 
+    st.write(result)
